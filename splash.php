8ff84c11774657e7edfa90a61f6e59d1ec866623
juvaly <juvaly@gmail.com>
2015-02-02 10:18:41 +0200

    Echo hotspot name

--- splash.php
+++ splash.php
@@ -12,8 +12,9 @@ $device_id = isset($_GET[PARAM_DEVICE_ID]) ? $_GET[PARAM_DEVICE_ID] : false;
 if ($device_id) {
 	$query = 'SELECT * FROM devices WHERE device_id=\''. $device_id. '\'';
 	$result = mysql_query($query) or die('query failed: ' . mysql_error());
+	$device = mysql_fetch_array($result);
 }
 
 ?>
 
-<h1>Welcome to </h1><?php print_r($result) ?>
+<h1>Welcome to </h1><?php echo $device['hotspot_name'] ?>
