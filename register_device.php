85d91d4becca313c3fa3beeb2c95185c400b80ee
juvaly <juvaly@gmail.com>
2015-02-02 11:10:05 +0200

    now?

--- register_device.php
+++ register_device.php
@@ -15,7 +15,7 @@ $hotspot_name = isset($_POST[PARAM_HOTSPOT_NAME]) ? $_POST[PARAM_HOTSPOT_NAME] :
 if (!($device_id)) die('device_id is required');
 
 $query = "INSERT INTO devices (device_id, hotspot_name) VALUES ('$device_id','$hotspot_name')";
-$result = mysql_query($query) or echo('insert failed: ' . mysql_error());
+//$result = mysql_query($query) or echo('insert failed: ' . mysql_error());
 echo $query;
 
 //header('Location: http://www.hotspotsplashscreens.com/hotspot-splash/splash.php?device_id'.$device_id);
