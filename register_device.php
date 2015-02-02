a2b6d6f26e4b8c05768c0860fdefc91cdff0c086
juvaly <juvaly@gmail.com>
2015-02-02 15:57:11 +0200

    perfect

--- register_device.php
+++ register_device.php
@@ -20,6 +20,6 @@ if (!($device_id)) die('device_id is required');
 $query = "INSERT INTO devices (device_id, hotspot_name) VALUES ('$device_id','$hotspot_name')";
 $result = mysql_query($query) or die('insert failed: ' . mysql_error());
 
-header('Location: http://www.hotspotsplashscreens.com/hotspot-splash/splash.php?device_id'.$device_id);
+header('Location: http://www.hotspotsplashscreens.com/hotspot-splash/splash.php?device_id='.$device_id);
 http_response_code(201);
 ?>
