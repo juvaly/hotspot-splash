51930a31cad606080187d85e12fe2b3a236160e9
juvaly <juvaly@gmail.com>
2015-02-02 11:00:29 +0200

    tetsing

--- register_device.php
+++ register_device.php
@@ -9,16 +9,16 @@ $dbname = 'hotspot-splash';
 $con = mysql_connect($server, $user, $pass) or die("Can't connect");
 mysql_select_db($dbname);
 
-$device_id = isset($_GET[PARAM_DEVICE_ID]) ? $_GET[PARAM_DEVICE_ID] : false;
+$device_id = isset($_POST[PARAM_DEVICE_ID]) ? $_POST[PARAM_DEVICE_ID] : false;
 $hotspot_name = isset($_POST[PARAM_HOTSPOT_NAME]) ? $_POST[PARAM_HOTSPOT_NAME] : 'Hotspot';
 
 echo($device_id);
 
-if (!($device_id)) echo('device_id is required');
+if (!($device_id)) die('device_id is required');
 
-//$query = "INSERT INTO devices (device_id, hotspot_name) VALUES ('". $device_id. "','". $hotspot_name ."')";
-//mysql_query($query) or echo('insert failed: ' . mysql_error());
+$query = "INSERT INTO devices (device_id, hotspot_name) VALUES ('". $device_id. "','". $hotspot_name ."')";
+mysql_query($query) or echo('insert failed: ' . mysql_error());
 
-//header('Location: http://www.hotspotsplashscreens.com/hotspot-splash/splash.php?device_id'.$device_id);
-//http_response_code(201);
+header('Location: http://www.hotspotsplashscreens.com/hotspot-splash/splash.php?device_id'.$device_id);
+http_response_code(201);
 ?>
