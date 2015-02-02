4055043e9e2fe318e5b49fcc0968f0ecabe50ba7
juvaly <juvaly@gmail.com>
2015-02-02 09:53:57 +0200

    Connect to database

--- splash.php
+++ splash.php
@@ -1,5 +1,10 @@
 <?php
-// load device info from database
+$server = 'localhost';
+$user = 'root';
+$pass = 'QEVk0C4uOVln';
+$dbname = 'hotspot-splash';
+$con = mysql_connect($server, $user, $pass) or die("Can't connect");
+mysql_select_db($dbname);
 ?>
 
 <h1>Welcome to </h1>
