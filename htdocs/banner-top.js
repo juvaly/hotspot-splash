3426ca3c7e9cdf8a94984c68af72b2fb5d539881
juvaly <juvaly@gmail.com>
2015-02-09 09:28:15 +0200

    dont display banner if in iframe

--- htdocs/banner-top.js
+++ htdocs/banner-top.js
@@ -9,7 +9,7 @@
 })();
 
 (function($) {
-	if ( self !== top ) {
+	if ( self === top ) {
 		var banner = $('<div id="hscp-banner"><h1 id="hscp-banner-title">my test banner</h1></div>');
 		$('body').prepend(banner);
 		$('#hscp-banner').css({ 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' });
