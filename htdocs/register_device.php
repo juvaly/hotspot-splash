b29a7e925716c4edbe69e31aa3d4463a7fe0398b
juvaly <juvaly@gmail.com>
2015-02-10 19:43:03 +0200

    more infor on device registration

--- htdocs/register_device.php
+++ htdocs/register_device.php
@@ -6,6 +6,9 @@ define("PARAM_DEVICE_ID", 'device_id');
 define("PARAM_HOTSPOT_NAME", 'hotspot_name');
 define("PARAM_SSID", 'source_ssid');
 define("PARAM_SSID_PWD", 'source_ssid_pwd');
+define("PARAM_REDIRECT_URL", 'redirect_url');
+define("PARAM_ENABLE_SPLASH", 'is_splash_enabled');
+define("PARAM_SALT", 'salt');
 
 $server = 'localhost';
 $user = 'root';
@@ -18,6 +21,8 @@ $device_id = isset($_POST[PARAM_DEVICE_ID]) ? $_POST[PARAM_DEVICE_ID] : false;
 $hotspot_name = isset($_POST[PARAM_HOTSPOT_NAME]) ? $_POST[PARAM_HOTSPOT_NAME] : 'Hotspot';
 $ssid = isset($_POST[PARAM_SSID]) ? $_POST[PARAM_SSID] : '';
 $ssid_pwd = isset($_POST[PARAM_SSID_PWD]) ? $_POST[PARAM_SSID_PWD] : '';
+$redirect_url = isset($_POST[PARAM_REDIRECT_URL]) ? $_POST[PARAM_REDIRECT_URL] : '';
+$is_splash_enabled = isset($_POST[PARAM_ENABLE_SPLASH]) ? $_POST[PARAM_ENABLE_SPLASH] : 1;
 
 if (!($device_id)) {
 	die('device_id is required');
@@ -28,11 +33,18 @@ if (!($device_id)) {
 $query = "SELECT device_id FROM devices WHERE device_id='$device_id'";
 $result = mysql_query($query) or die('failed testing device exists: ' . mysql_error());
 if (mysql_fetch_array($result) !== false) {
-	$query = "UPDATE devices  SET hotspot_name = '$hotspot_name', ssid = '$ssid', ssid_pwd = '$ssid_pwd' WHERE device_id = '$device_id'";
+	$query = "UPDATE devices SET 
+	hotspot_name = '$hotspot_name', 
+	ssid = '$ssid', 
+	ssid_pwd = '$ssid_pwd',
+	is_splash_enabled = $is_splash_enabled,
+	salt = $salt
+	WHERE device_id = '$device_id'";
 }
 else
 {
-	$query = "INSERT INTO devices (device_id, hotspot_name, ssid, ssid_pwd) VALUES ('$device_id','$hotspot_name','$ssid','$ssid_pwd')";
+	$query = "INSERT INTO devices (device_id, hotspot_name, ssid, ssid_pwd, is_splash_enabled, redirect_url, salt) 
+	VALUES ('$device_id','$hotspot_name','$ssid','$ssid_pwd', $is_splash_enabled, '$redirect_url', '$salt')";
 }
 
 // register or update device
--- htdocs/splash.php
+++ htdocs/splash.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
@@ -7,7 +8,7 @@ header('Pragma: no-cache');
 define("PARAM_DEVICE_ID", 'device_id');
 
 $authaction = $_GET['authaction'];
-$redir = $_GET['redir'];
+$original_redirect = $_GET['redir'];
 $tok = $_GET['tok'];
 
 $server = 'localhost';
@@ -24,11 +25,36 @@ if ($device_id) {
 	$device = mysql_fetch_array($result);
 }
 
-$url = "$authaction?redir=" . urlencode($redir) . "&tok=$tok";
+$auth_url = "$authaction?redir=" . urlencode($original_redirect) . "&tok=$tok";
 
 $hotspot_name = isset($device) ? $device['hotspot_name'] : 'Nowhere';
+$redirect_url = isset($device) ? $device['redirect_url'] : '';
+$is_splash_enabled = isset($device) ? $device['is_splash_enabled'] : 0;
+
+// don't display any splash page
+if (!($is_splash_enabled)) {
+	header('Location: ' . $auth_url, true, 302);
+	exit();
+}
 
 ?>
 
-<h1>Welcome to <?php echo $hotspot_name ?></h1>
-<a href="<?= $url ?>" target="_top">Accept Terms</a>
+<!-- page output -->
+<html>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="text-center">
+				<h1>Welcome to <?php echo $hotspot_name ?></h1>
+				<a href="<?= $auth_url ?>" target="_top">Accept Terms</a>
+			</div>
+		</div>
+
+		<!-- scripts -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	</body>
+
+</html>
