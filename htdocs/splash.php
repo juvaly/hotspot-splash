21ad5619ae79a1a0bd334c226bf04f1ccac050d1
juvaly <juvaly@gmail.com>
2015-02-11 11:08:22 +0200

    splash layout changes

--- htdocs/splash.php
+++ htdocs/splash.php
@@ -62,17 +62,23 @@ if (!($is_splash_enabled)) {
 
 						<h2 class="text-primary">המומלצים שלנו היום:</h2>
 					</div>
-					<div class="col-md-3 text-center bg-warning">
-						<h3>קפה + מאפה</h2>
-						<h2><strong>16 ₪</strong></h1>
+					<div class="col-md-3 text-center">
+						<div class="bg-warning">
+							<h3>קפה + מאפה</h2>
+							<h2><strong>16 ₪</strong></h1>
+						</div>
 					</div>
 					<div class="col-md-3 col-md-offset-1 text-center bg-warning">
-						<h3>קפה + סנדביץ׳ ביס</h2>
-						<h2><strong>22 ₪</strong></h1>
+						<div class="bg-warning">
+							<h3>קפה + סנדביץ׳ ביס</h2>
+							<h2><strong>22 ₪</strong></h1>
+						</div>
 					</div>
 					<div class="col-md-3 col-md-offset-1 text-center bg-warning">
-						<h3>עסקית צהריים</h2>
-						<h2><strong>59 ₪</strong></h1>
+						<div class="bg-warning">
+							<h3>עסקית צהריים</h2>
+							<h2><strong>59 ₪</strong></h1>
+						</div>
 					</div>
 			</div>
 
