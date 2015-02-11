a132f017099b1b8899f4e492e5a643cb585aa59f
juvaly <juvaly@gmail.com>
2015-02-11 17:39:05 +0200

    local resources for splash screen

--- htdocs/splash.php
+++ htdocs/splash.php
@@ -48,7 +48,7 @@ if (!($is_splash_enabled)) {
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<title><?php echo $hotspot_name ?></title>
 
 		<style>
@@ -110,8 +110,8 @@ if (!($is_splash_enabled)) {
 		</div>
 
 		<!-- scripts -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</body>
 
 </html>
