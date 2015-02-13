7e544fb2ec9943294ae3048aab22136123d9a4fb
juvaly <juvaly@gmail.com>
2015-02-13 14:30:10 +0200

    no jquery in test banner

--- htdocs/banner-top.js
+++ htdocs/banner-top.js
@@ -1,12 +1,9 @@
 (function() {
-	if(!window.jQuery)
-	{
-	   var css = document.createElement('style');
-	   css.innerHTML = 
-   		"#hscp-banner { 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' }" +
-   		"#hscp-banner-title { color: '#900', padding: '20px' }";
-	   document.getElementsByTagName('head')[0].appendChild(css);
-	}
+	var css = document.createElement('style');
+	css.innerHTML = 
+		"#hscp-banner { 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' }" +
+		"#hscp-banner-title { color: '#900', padding: '20px' }";
+	document.getElementsByTagName('head')[0].appendChild(css);
 
 	if ( self === top ) {
 		var banner = document.createElement('div');
@@ -14,6 +11,7 @@
 		var title = document.createElement('h1');
 		title.id = 'hscp-banner-title';
 		banner.appendChild(title);
-		document.getElementsByTagName('body')[0].prependChild(banner);
+		var body = document.getElementsByTagName('body')[0];
+		body.insertBefore(banner, body.firstChild);
 	}
 })();
