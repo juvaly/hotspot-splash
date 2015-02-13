86a179b2021f2eb3efab5f71fa28e439d0e51b90
juvaly <juvaly@gmail.com>
2015-02-13 14:06:10 +0200

    trying to be more generic with test banner

--- htdocs/banner-top.js
+++ htdocs/banner-top.js
@@ -4,15 +4,13 @@
 	   var script = document.createElement('script');
 	   script.type = "text/javascript";
 	   script.src = "https://code.jquery.com/jquery-2.1.3.min.js";
-	   document.getElementsByTagName('head')[0].appendChild(script);
+	   document.getElementsByTagName('body')[0].appendChild(script);
 	}
 
-	$(document).ready(function() {
-		if ( self === top ) {
-			var banner = $('<div id="hscp-banner"><h1 id="hscp-banner-title">my test banner</h1></div>');
-			$('body').prepend(banner);
-			$('#hscp-banner').css({ 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' });
-			$('#hscp-banner-title').css({ color: '#900', padding: '20px' });
-		}
-	});
+	if ( self === top ) {
+		var banner = $('<div id="hscp-banner"><h1 id="hscp-banner-title">my test banner</h1></div>');
+		$('body').prepend(banner);
+		$('#hscp-banner').css({ 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' });
+		$('#hscp-banner-title').css({ color: '#900', padding: '20px' });
+	}
 })();
