c822020f0386dc80084cde0a9682fe8537367a9f
juvaly <juvaly@gmail.com>
2015-02-13 14:09:06 +0200

    trying to be more generic with test banner

--- htdocs/banner-top.js
+++ htdocs/banner-top.js
@@ -4,13 +4,13 @@
 	   var script = document.createElement('script');
 	   script.type = "text/javascript";
 	   script.src = "https://code.jquery.com/jquery-2.1.3.min.js";
-	   document.getElementsByTagName('body')[0].appendChild(script);
+	   document.getElementsByTagName('head')[0].appendChild(script);
 	}
 
 	if ( self === top ) {
-		var banner = $('<div id="hscp-banner"><h1 id="hscp-banner-title">my test banner</h1></div>');
-		$('body').prepend(banner);
-		$('#hscp-banner').css({ 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' });
-		$('#hscp-banner-title').css({ color: '#900', padding: '20px' });
+		var banner = jQuery('<div id="hscp-banner"><h1 id="hscp-banner-title">my test banner</h1></div>');
+		jQuery('body').prepend(banner);
+		jQuery('#hscp-banner').css({ 'min-height': '90px', 'text-align': 'center', 'background': '#fff', 'border': 'solid 10px #0f0' });
+		jQuery'#hscp-banner-title').css({ color: '#900', padding: '20px' });
 	}
 })();
