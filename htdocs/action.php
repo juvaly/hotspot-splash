b98a920a59117a1c333e110b1c8f60c2bb306b70
juvaly <juvaly@gmail.com>
2015-02-15 11:58:26 +0200

    tracking script

---
+++ htdocs/action.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL); 
+ini_set("display_errors", 1); 
+
+header('Content-Type: text/javascript');
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
+define("PARAM_DEVICE_ID", 'd_id');
+define("PARAM_CLIENT_ID", 'c_id');
+define("PARAM_TOKEN", 't');
+define("PARAM_ACTION", 'a');
+
+# get parameters
+$device_id = isset($_GET[PARAM_DEVICE_ID]) ? $_GET[PARAM_DEVICE_ID] : false;
+$client_id = isset($_GET[PARAM_CLIENT_ID]) ? $_GET[PARAM_CLIENT_ID] : false;
+$token = isset($_GET[PARAM_TOKEN]) ? $_GET[PARAM_TOKEN] : false;
+$action = isset($_GET[PARAM_ACTION]) ? $_GET[PARAM_ACTION] : false;
+
+# fail if missing
+
+# connect to database
+$server = 'localhost';
+$user = 'root';
+$pass =  'QEVk0C4uOVln';
+$dbname = 'hotspot-splash';
+$con = mysql_connect($server, $user, $pass) or die("Can't connect");
+mysql_select_db($dbname);
+
+# response json
+$response_json = '{ status: "OK" }'
+echo $response_json;
+?>
--- htdocs/banner-top.js
+++ htdocs/banner-top.js
@@ -1,18 +1,57 @@
 (function() {
-	var link = document.createElement("link");
-	link.href = "http://www.hotspotsplashscreens.com/hotspot-splash/banner-top.css";
-	link.type = "text/css";
-	link.rel = "stylesheet";
-	document.getElementsByTagName("head")[0].appendChild(link)
-;
-	if ( self === top ) {
-		var banner = document.createElement('div');
-		banner.id = "hscp-banner";
-		var title = document.createElement('h1');
-		title.id = "hscp-banner-title";
-		title.innerHTML = "my test banner";
-		banner.appendChild(title);
-		var body = document.getElementsByTagName('body')[0];
-		body.insertBefore(banner, body.firstChild);
+//	var link = document.createElement("link");
+//	link.href = "http://www.hotspotsplashscreens.com/hotspot-splash/banner-top.css";
+//	link.type = "text/css";
+//	link.rel = "stylesheet";
+//	document.getElementsByTagName("head")[0].appendChild(link);
+
+//	if ( self === top ) {
+//		var banner = document.createElement("div");
+//		banner.id = "hscp-banner";
+//		var title = document.createElement("h1");
+//		title.id = "hscp-banner-title";
+//		title.innerHTML = "my test banner";
+//		banner.appendChild(title);
+//		var body = document.getElementsByTagName("body")[0];
+//		body.insertBefore(banner, body.firstChild);
+//	}
+
+//
+function parseQueryString() {
+	var str = window.location.search;
+
+	str = str.trim().replace(/^(\?|#)/, '');
+
+	if (!str) {
+		return {};
 	}
+
+	return str.trim().split('&').reduce(function (ret, param) {
+		var parts = param.replace(/\+/g, ' ').split('=');
+		var key = parts[0];
+		var val = parts[1];
+
+		key = decodeURIComponent(key);
+		// missing `=` should be `null`:
+		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+		val = val === undefined ? null : decodeURIComponent(val);
+
+		if (!ret.hasOwnProperty(key)) {
+			ret[key] = val;
+		} else if (Array.isArray(ret[key])) {
+			ret[key].push(val);
+		} else {
+			ret[key] = [ret[key], val];
+		}
+
+		return ret;
+	}, {});
+};
+
+//
+var qs = parseQueryString();
+var url = "http://www.hotspotsplashscreens.com/hotspot-splash/action.php?a=trk&t=" + qs.t + "d_id=" + qs.d_id + "c_id" = qs.c_id;
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
 })();
